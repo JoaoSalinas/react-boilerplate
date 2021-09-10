97e4ccdaa6608cfb4850d1455ef6dd2e548df720
JoaoSalinas <joao.salinas@outlook.com>
2021-09-10 15:29:19 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
+export default function Home({ title = 'React Avançado' }) {
+  const teste = "";
 
-export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {title}
         </h1>
 
         <p className={styles.description}>
